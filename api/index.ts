dd1436001afd066d94402b036b94b303c512f2dd
mtonon <math.tonon@gmail.com>
2025-05-06 13:55:57 -0300

    refact: move all server files into api folder

---
+++ api/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import paymentsRoutes from './src/routes/payments';
+import registrationsRoutes from './src/routes/registrations';
+
+dotenv.config();
+
+const app = express();
+const port = process.env['PORT'] || 3000;
+
+// Middleware
+app.use(cors({
+  origin: ['https://vilakids-inscricoes.vercel.app', 'http://localhost:4200'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+app.options('*', cors());
+
+app.use(express.json());
+
+// Routes
+app.use('/api/payments', paymentsRoutes);
+app.use('/api/registrations', registrationsRoutes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
