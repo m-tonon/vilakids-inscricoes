b590c1c40f26004235b30084ef1bf38a5ff048a4
mtonon <math.tonon@gmail.com>
2025-05-14 06:35:08 -0300

    feat: add new page to display the registrations for admin user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
+import { RegistrationsViewComponent } from './registrations-view/registrations-view.component';
 
 export const routes: Routes = [
   { path: '', component: RegistrationComponent },
-  { path: 'register', component: RegistrationComponent }
+  { path: 'register', component: RegistrationComponent },
+  { path: 'registrations-view', component: RegistrationsViewComponent },
 ];
